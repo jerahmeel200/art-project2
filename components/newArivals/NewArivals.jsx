d3cea85a36bb7b0d8c782225078e3269e8c1c0c0
areaboxer <areaboxer@gmail.com>
2023-08-22 21:02:19 +0100

    update

--- components/newArivals/NewArivals.jsx
+++ components/newArivals/NewArivals.jsx
@@ -39,7 +39,7 @@ function NewArivals() {
         <div className={styles.right}>
           <div className={styles.desc}>
             <h4>NEW ARIVAL</h4>
-            <h1>"Smoke Break" Barack Obama & MLK</h1>
+            <h1>Smoke Break Barack Obama & MLK</h1>
             <h5 className="text-1xl font-bold  ">$169.00</h5>
           </div>
 
@@ -69,54 +69,3 @@ function NewArivals() {
 }
 
 export default NewArivals;
-
-// "use client";
-// import React, { useState } from "react";
-
-// function NewArivals() {
-//   const imgs = [
-//     { id: 0, value: "/images/img-1.png" },
-//     { id: 1, value: "/images/img-2.jpg" },
-//     { id: 2, value: "/images/img-3.png" },
-//   ];
-//   const [sliderData, setSliderData] = useState([imgs[0]]);
-
-//   const handleImageClick = (index) => {
-//     const slider = [imgs[index]];
-//     setSliderData(slider);
-//   };
-
-//   return (
-//     <div className="bg-indigo-300 flex flex-wrap p-10">
-//       <div className="w-full md:w-1/2 bg-orange-900 p-4 flex   ">
-//         <div className="   ">
-//           {imgs.map((item, i) => (
-//             <img
-//               className={`cursor-pointer ${
-//                 sliderData[0].id === i ? "border-2 border-white" : ""
-//               }`}
-//               key={item.id}
-//               height="150"
-//               width="70"
-//               src={item.value}
-//               alt="thumbnail"
-//               onClick={() => handleImageClick(i)}
-//             />
-//           ))}
-//         </div>
-//         <div className="md:w-full md:pl-6">
-//           <img
-//             src={sliderData[0].value}
-//             alt="large"
-//             className="w-full h-auto md:w-auto md:h-96"
-//           />
-//         </div>
-//       </div>
-//       <div className="w-full md:w-1/2 p-4">
-//         <div>hay</div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default NewArivals;
