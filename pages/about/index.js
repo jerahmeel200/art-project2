b75a17be6282bbd37fa81d12374ab8406a760ab5
areaboxer <areaboxer@gmail.com>
2023-08-25 16:01:13 +0100

    create about page but still in progress

---
+++ pages/about/index.js
@@ -0,0 +1,94 @@
+import React from "react";
+import styles from "./about.module.css";
+function index() {
+  return (
+    <div style={{ paddingTop: "70px" }} className="main">
+      <div className={styles.title}>
+        <h1> ABOUT</h1>
+      </div>
+      <div className={styles.descCont}>
+        <div className={styles.desc}>
+          <h1>NGABO’ EL’CESART” DESIRE</h1>
+          <p>
+            {" "}
+            Who El’Cesart ? Spirited, Skilled And Sincere Are Only A Few Words
+            To Describe The Upcoming Artist. Ngabo D.Cesar Also Known As
+            El’Cesart Is A 27 years old Visual
+          </p>
+          <p>
+            Artist & Illustrator Creating Honest Work Celebrating Black People,
+            Culture And Afro-Futurism. His Work Takes The Form Of Digital
+            Illustration, Paint, Apparel Designing, And Photography. El’Cesart
+            Explores Pop Society, Turning It Into An Intriguing Globe Of Bold
+            Colours, Textures And Prints. Uplifted And Inspired By Black Culture
+            His Work Brings Together
+          </p>
+
+          <p>
+            Those Who Articulate A Healthy Relationship With Life. His Work
+            Features Characters From Music Artists And Film Stars To Raw
+            Beautiful Women And Men. Connecting With Viewers Who Understand The
+            Concept Of Cultural Aesthetic.
+          </p>
+          <p>
+            {" "}
+            Nudity Can Be Expected In His Magnetic Work. Initially Wanting A
+            Career As An Animator Or Concept Artist For Games And Comic Books,
+            El’Cesart Is An Emerging Influencer Of The New Era Inspired To Push
+            Boundaries Within Art Representing Black Africans. Gaining Over
+            Forty Thousand Fans From Around The World On Instagram His Artwork
+            Has Gathered Many Praises Including A-List Celebrities From Michael
+            B.Jordan, Lupita Nyong’o, Anderson Paak, Jaden And Willow Smith, T-
+            Pain, Ty Dolla $ign, Just To Name a Few.
+          </p>
+          <p>
+            {" "}
+            Although The Story Of This 27-Year Old May Sound Like A Typical
+            Teenager Graduating From High School And Attending College- His Work
+            Is Very Far From The Norm. Staying True To His Vision And Beliefs Of
+            Shedding Light On Communities Of Black Africans, El’Cesart Has
+            Launched His Own Streetwear Collection That You Can Purchase
+            Straight From His Website. Taking Steps Into The Fashion Industry
+            Was Yet Another Strong Method To Expand His Parameters As An Artist
+            And Creative Designer.{" "}
+          </p>
+          <p>
+            El’Cesart Has Taken A Bigger Steps By Starting To Collaborate With
+            Known Hollywood entertainment companies, TV and Magazines Such As
+            H.B.O, Amazon Prime, HULU, BLEACHERS REPORT, Revolt TV, and
+            particularly Annapurna Studio A Production House Where By He Created
+            Amazing Artworks For Their Oscar Winning Movie''If Beale Street
+            Could Talk'' A Movie Directed By The Oscar Winning Director Barry
+            Jenkins. El'Cesart Also worked on other films like Lovecraft
+            Country, Phat Tuesday Documentary, The U.S vs Billie Holiday, etc..{" "}
+          </p>
+          <p>
+            {" "}
+            Private Buyers Can Also Pay For Commissioned Artwork, In Which They
+            Automatically Become The Almighty Painted On A Canvas Of Their Own
+            Body And Face. Desire’s Bright And Detailed Interpretations Show His
+            Persistent Pure Motives To Empower Black Culture And Help The
+            Audience Connect With Their Experiences. He Addresses The
+            Representation Of Black Beauty, Sexuality And Power In Today’s
+            World. El’Cesart Is Defiantly Rare In Rendering Paint Against Canvas
+            And Digital Illustration, But What Really Sets His Work Apart Is Its
+            Celebratory Soul, That Each Piece Of Work Showcases.
+          </p>
+          <p>
+            {" "}
+            Desire’s Work Offers Inspiration, Interest And Thought. From Here
+            Onwards He Will Continue To Invite The Audience On To A Trip Through
+            His Artwork, Photographs And Designs, For Those Who Join Prepare To
+            Encounter A Thrilling Journey
+          </p>
+        </div>
+
+        <div className={styles.image}>
+          <img src="/images/headShot.webp" alt="head-shot" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default index;
